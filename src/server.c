8ecc316419927fd9d5654a3ffc305ffb12b0dae5
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-11-20 16:50:11 -0300

    update: Adicionados comentários explicativos

--- src/server.c
+++ src/server.c
@@ -1,5 +1,10 @@
 #include "server.h"
 
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
 int socket_creation(int n_connections){
 
     bool check = true;
@@ -33,6 +38,7 @@ response *load_file(char *path){
     FILE *reader = fopen(path, "r");
     response *file = (response *) malloc(sizeof(response));
 
+    // Caso em que o arquivo não é encontrado (404)
     if (reader == NULL){
 
         printf("ERROR: Not found!\n");
@@ -55,6 +61,7 @@ response *load_file(char *path){
     file->message_length = ftell(reader);
     file->response_buffer = malloc(file->message_length);
 
+    // Caso em que não existe memória suficiente para o arquivo (413)
     if (file->response_buffer == NULL){
 
         printf("ERROR: Payload too large!\n");
@@ -74,6 +81,7 @@ response *load_file(char *path){
         return file;
     }
 
+    // Caso geral: carrega o arquivo solicitado
     rewind(reader);
     fread(file->response_buffer, 1, file->message_length, reader);
     fclose(reader);
@@ -86,6 +94,8 @@ response *http_parser(request *r){
 
     bool check = true;
     int i, j, h_size;
+
+    // Arrays para manipular as mensagens
     char method[4];
     char header[128] = {0};
 
@@ -95,6 +105,7 @@ response *http_parser(request *r){
     response *file;
     response *answer = (response *) malloc(sizeof(response));
 
+    // Checa se o método da linha de requisição é o GET
     for (i = 0; r->header[i] != ' '; i++)
         method[i] = r->header[i];
     method[i] = r->header[i];
@@ -102,10 +113,12 @@ response *http_parser(request *r){
     if (strcmp(method, "GET ") != 0)
         check = false;
 
+    // Caso em que a requisição está bem formatada
     if (check) {
         i++;
         j = 0;
 
+        // Obtém o caminho do arquivo na url
         while (r->header[i] != ' ') {
 
             if (j > 0 || r->header[i] != '/') {
@@ -117,10 +130,12 @@ response *http_parser(request *r){
 
         DIR *d = opendir(path);
 
+        // Caso em que o arquivo requisitado é index.html ou outro qualquer
         if (path[0] == '/' && path[1] == 0 || path[0] == 0 || d != NULL) {
             closedir(d);
             file = load_file("../files/404.html");
         } else {
+            // Caso em que o caminho não possui o prefixo '../files/'
             if ((int) strncmp("../files/", path, 9)) {
                 char tmp[PATH_SIZE] = {0};
                 sprintf(tmp, "../files/%s", path);
@@ -144,6 +159,7 @@ response *http_parser(request *r){
         fclose(reader);
     }
 
+    // Geração das respostas
     if (file->status == 404)
         sprintf(header, "HTTP/1.1 404 Not Found\nContent-Length: %d\r\n\r\n", file->message_length);
     else if (file->status == 413)
@@ -178,6 +194,7 @@ request *read_header(int client_socket){
 
 void client_response(int client_socket_fd){
 
+    // Recebe a linha de requisição da mensagem HTTP para realizar o parsing
     request *r = read_header(client_socket_fd);
     struct response *answer = http_parser(r);
 
@@ -195,11 +212,15 @@ void client_response(int client_socket_fd){
 
 void iterative_server(){
 
+    // Variáveis para comunicação em rede (sockets)
     int client_socket_fd;
     struct sockaddr_in client;
     int size_socket_addr = sizeof(struct sockaddr_in);
+
+    // Somente uma requisição por vez
     int socket_fd = socket_creation(1);
 
+    // LOOP INFINITO: Servidor recebendo requisições indefinidamente
     while (true) {
         client_socket_fd = accept(socket_fd, (struct sockaddr *) &client, (socklen_t *) &size_socket_addr);
 
@@ -221,6 +242,7 @@ void fork_server() {
     int socket_fd = socket_creation(1);
     pid_t child;
 
+    // LOOP INFINITO: Servidor recebendo requisições indefinidamente
     while (true) {
         client_socket_fd = accept(socket_fd, (struct sockaddr *) &client, (socklen_t *) &size_socket_addr);
 
@@ -230,6 +252,7 @@ void fork_server() {
         if ((child = fork()) < 0)
             error("ERROR: Fork!\n");
 
+        // Processo filho processa a resposta da requisição HTTP do cliente
         if (child == 0) {
             client_response(client_socket_fd);
             sleep(5);
@@ -253,10 +276,13 @@ void consumer() {
 
         for (int i = 0; i < MAX_CONNECTIONS; i++) {
 
+            // Caso em que exista requisições na fila para serem respondidas
             if (!pthread_mutex_trylock(&queue_control[i])) {
 
                 if (requests_queue[i] != -1) {
                     client_response((int) requests_queue[i]);
+
+                    // Marcação da requisição como já atendida
                     requests_queue[i] = -1;
                 }
                 pthread_mutex_unlock(&queue_control[i]);
@@ -269,6 +295,7 @@ void consumer() {
                 pthread_mutex_unlock(&queue_control[BUFFER_SIZE - 1 - i]);
             }
         }
+        // Caso em que não há requisições para atender -> inativo por 1s
         sleep(1);
     }
 }
@@ -278,13 +305,16 @@ void thread_server() {
     int client_socket_fd;
     struct sockaddr_in client;
     int size_socket_addr = sizeof(struct sockaddr_in);
+
+    // Várias conexões pararelas
     int socket_fd = socket_creation(MAX_CONNECTIONS);
 
+    // Fila de requisições
     for (int i = 0; i < BUFFER_SIZE; i++)
         requests_queue[i] = -1;
 
+    // Criação das threads para consumir
     pthread_mutex_init(&n_control, NULL);
-
     for (int i = 0; i < MAX_CONNECTIONS; i++) {
 
         pthread_mutex_init(&queue_control[i], NULL);
@@ -292,6 +322,7 @@ void thread_server() {
             error("ERROR: Thread!\n");
     }
 
+    // LOOP INFINITO: Servidor recebendo requisições indefinidamente
     while (true) {
 
         client_socket_fd = accept(socket_fd, (struct sockaddr *) &client, (socklen_t *) &size_socket_addr);
@@ -301,6 +332,7 @@ void thread_server() {
 
         for (int i = 0; i < BUFFER_SIZE; i++) {
 
+            // Caso em que a requisição atual já foi atendida
             if (requests_queue[i] == -1) {
 
                 if (!pthread_mutex_trylock(&queue_control[i])) {
@@ -339,7 +371,10 @@ void concurrent_server() {
     FD_SET(socket_fd, &master);
     max_socket = socket_fd;
 
+    // LOOP INFINITO: Servidor recebendo requisições indefinidamente
     while (true) {
+
+        // Espera por novas requisições
         reader = master;
         activity = select(max_socket + 1, &reader, NULL, NULL, NULL);
 
@@ -348,7 +383,10 @@ void concurrent_server() {
 
         for (int i = 0; i <= max_socket; i++) {
 
+            // Caso em que alguma mensagem foi escrita no socket i
             if (FD_ISSET(i, &reader)) {
+
+                // Caso em que i é o socket do servidor para receber novas requisições
                 if (i == socket_fd) {
 
                     client_socket_fd = accept(socket_fd, (struct sockaddr *) &client, (socklen_t *) &size_socket_addr);
@@ -366,4 +404,17 @@ void concurrent_server() {
             }
         }
     }
+}
+
+void test_parser() {
+
+    // Teste do parser isolado
+    request *r = (request *) malloc(sizeof(request));
+    response *a = (response *) malloc(sizeof(response));
+
+    r->bytes_read = 0;
+    strcpy(r->header, TEST3);
+
+    a = http_parser(r);
+    printf("%s\n", a->response_buffer);
 }
--- src/server.h
+++ src/server.h
@@ -1,46 +1,61 @@
-#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <dirent.h>
-#include <pthread.h>
-#include <stdbool.h>
-#include <sys/wait.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdio.h>
 
+// CONSTANTES (nº da porta, máximo de conexões, tamanho máximo da linha de requisição HTTP (GET), URL e buffer)
 #define PORT_NUMBER 2000
 #define MAX_CONNECTIONS 16
 #define HEADER_SIZE 1024
 #define PATH_SIZE 128
 #define BUFFER_SIZE 128
 
+// MENSAGENS DE TESTE
+#define TEST1 "GET ../files/m.png HTTP/1.1"
+#define TEST2 "GET ../files/cat.jpg HTTP/1.1"
+#define TEST3 "GT ../files/f1.gif HTTP/1.1"
+
+// CONTROLE DAS THREADS
 pthread_mutex_t n_control;
 u_int32_t requests_queue[BUFFER_SIZE];
 pthread_mutex_t queue_control[BUFFER_SIZE];
 pthread_t threads[MAX_CONNECTIONS];
 
+// RESPOSTA
 typedef struct response {
     char *response_buffer;
     u_int32_t message_length;
     int status;
 } response;
 
+// REQUISIÇÃO
 typedef struct request {
     int bytes_read;
     char header[HEADER_SIZE];
 } request;
 
+// ACUSAÇÃO DE ERROS E CRIAÇÃO DAS CONEXÕES (SOCKETS)
 void error(const char *msg);
 int socket_creation(int n_connections);
 
+// MANIPULAÇÃO DAS MENSAGENS HTTP
 request *read_header(int client_socket);
 response *load_file(char *path);
 response *http_parser(request *r);
 void client_response(int client_socket_fd);
 
+// SERVIDORES
 void iterative_server();
 void fork_server();
 void consumer();
 void thread_server();
 void concurrent_server();
+
+// TESTE AUXILIAR
+void test_parser();
