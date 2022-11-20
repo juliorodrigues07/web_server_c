b73082d16b332c19fb964a5b87c4444d82a72884
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-11-20 16:49:47 -0300

    feat: Main finalizado

--- src/main.c
+++ src/main.c
@@ -2,41 +2,29 @@
 
 int main(int argc, char **argv) {
 
-    int option = atoi(argv[1]);
-
-    if (argc < 2)
+    if (argc < 2 || argc >= 3)
         error("USAGE: ./main <option>\n");
+
+    int option = atoi(argv[1]);
 
     switch (option) {
         case 1:
-            printf("eu\n");
             iterative_server();
             break;
         case 2:
-            printf("tu\n");
-            //fork_server();
+            fork_server();
             break;
         case 3:
-            printf("ele\n");
-            //thread_server();
+            thread_server();
             break;
         case 4:
-            printf("nós\n");
-            //select_server();
+            concurrent_server();
             break;
         default:
-            printf("Invalid Option!\n");
+            printf("ERROR: Invalid Option!\n");
             break;
     }
 
-    // Teste
-    request *r = (request *) malloc(sizeof(request));
-    response *a = (response *) malloc(sizeof(response));
-
-    r->bytes_read = 0;
-    strcpy(r->header, "GET ../files/m.png HTTP/1.1");
-
-    a = http_parser(r);
-    printf("%s\n", a->response_buffer);
+    //test_parser();
     return 0;
 }
